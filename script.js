11ed2e2a8ad0d714e4b96d629472b4d555aa8f35
patilprajwal9942-creator <patilprajwal9942@gmail.com>
2026-03-20 11:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
+	let str = "ABC";
+
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i], str.charCodeAt(i));
+}
   // write your code here
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
+const str = prompt("prajwal");
 alert(byteSize(str));
